8c6e4bc61a9a7d7c248f04d34db4eee8078a33d9
jluisalegria <luis@futurointeligente.com>
2016-03-29 12:14:47 -0600

    Added files via upload

---
+++ WirelessBoard/MiWi_Rich_Features_Node1.X/build/default/production/Cisternas Unison 2.0.o.d
@@ -0,0 +1,32 @@
+build/default/production/Cisternas\ Unison\ 2.0.o:  \
+	C:/Users/Pedro/MPLABXProjects/MiWi_Rich_Features_Node1.X/Cisternas\ Unison\ 2.0.c  \
+	C:/Users/Pedro/MPLABXProjects/MiWi_Rich_Features_Node1.X/HardwareProfile.h  \
+	C:/CodeBank8/bsp/Microchip/gpios/gpios.h  \
+	C:/CodeBank8/bsp/Microchip/spi/spi.h  \
+	C:/CodeBank8/modules/st7032/st7032.h  \
+	C:/CodeBank8/bsp/Microchip/uart1/uart1.h  \
+	C:/CodeBank8/bsp/Microchip/uart1/uart2.h  \
+	C:/CodeBank8/middleware/printf/printf.h  \
+	C:/CodeBank8/bsp/Microchip/delays/delays.h  \
+	C:/CodeBank8/bsp/Microchip/system/system.h  \
+	C:/microchip_solutions_v2013-06-15/Microchip/Include/Transceivers/Security.h  \
+	C:/microchip_solutions_v2013-06-15/Microchip/Include/Transceivers/MCHP_MAC.h  \
+	C:/microchip_solutions_v2013-06-15/Microchip/Include/TimeDelay.h  \
+	C:/microchip_solutions_v2013-06-15/Microchip/Include/WirelessProtocols/MiWiPRO/MiWiPRO.h  \
+	C:/microchip_solutions_v2013-06-15/Microchip/Include/WirelessProtocols/NVM.h  \
+	C:/microchip_solutions_v2013-06-15/Microchip/Include/WirelessProtocols/MCHP_API.h  \
+	C:/Users/Pedro/MPLABXProjects/MiWi_Rich_Features_Node1.X/MAC_EEProm.h  \
+	C:/Users/Pedro/MPLABXProjects/MiWi_Rich_Features_Node1.X/SST_SerialFlash.h  \
+	C:/microchip_solutions_v2013-06-15/Microchip/Include/WirelessProtocols/Console.h  \
+	C:/Users/Pedro/MPLABXProjects/MiWi_Rich_Features_Node1.X/DemoOutput.h  \
+	C:/CodeBank8/types.h  \
+	C:/Users/Pedro/MPLABXProjects/MiWi_Rich_Features_Node1.X/ConfigApp.h  \
+	C:/microchip_solutions_v2013-06-15/Microchip/Include/GenericTypeDefs.h  \
+	C:/Users/Pedro/MPLABXProjects/MiWi_Rich_Features_Node1.X/SystemProfile.h  \
+	C:/microchip_solutions_v2013-06-15/Microchip/Include/WirelessProtocols/SymbolTime.h  \
+	C:/microchip_solutions_v2013-06-15/Microchip/Include/Compiler.h  \
+	C:/microchip_solutions_v2013-06-15/Microchip/Include/Transceivers/Transceivers.h  \
+	C:/microchip_solutions_v2013-06-15/Microchip/Include/Transceivers/MRF89XA/MRF89XA.h  \
+	C:/microchip_solutions_v2013-06-15/Microchip/Include/Transceivers/MRF89XA/ConfigMRF89XA.h  \
+	C:/microchip_solutions_v2013-06-15/Microchip/Include/WirelessProtocols/MiWiPRO/ConfigMiWiPRO.h  \
+
